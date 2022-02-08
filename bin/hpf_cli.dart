7d82ad79b3104f5825a760163cf287fab75862e0
hoan-dovan1 <hoan.dovan1@vti.com.vn>
2022-02-08 11:27:55 +0700

    (bug) - remove unused code

--- bin/hpf_cli.dart
+++ bin/hpf_cli.dart
@@ -4,10 +4,9 @@ import 'package:hpf_cli/core/generator.dart';
 void main(List<String> arguments) async {
   var time = Stopwatch();
   time.start();
+
   final command = HpfCli(arguments).findCommand();
   await command.execute();
-
-  await ShellUtils.pubGet();
   await ShellUtils.buildX();
 
   time.stop();
